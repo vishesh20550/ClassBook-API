5250e1e157ef752189cc2576adaf029c3c682b89
Vishesh Jain <vishesh20550@iiitd.ac.in>
2024-09-14 15:56:06 +0530

    app update

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def convert_pdf():
     data = request.get_json()
-    pdf_url = data['pdfUrl']
+    pdf_id = data['pdfId']
 
     # Call your model function to convert PDF to text
-    extracted_text = process_pdf_from_url(pdf_url)
+    extracted_text = process_pdf_from_url(pdf_id)
 
     return jsonify({'script': extracted_text})
 
